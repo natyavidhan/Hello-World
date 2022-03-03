e1c32c699461dc46a284a35bbf7c70308bcd830e
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-03-03 19:34:17 +0530

    C Sharp

---
+++ main.cs
@@ -0,0 +1,9 @@
+using System;
+namespace Main {
+    class SGDoesCS {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
